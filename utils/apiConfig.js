690c3509968515e04c7b46cd9f3edb4029839b36
Rahul Yadav <158089272+raybalawas@users.noreply.github.com>
2025-10-06 12:23:27 +0530

    Update apiConfig.js

--- utils/apiConfig.js
+++ utils/apiConfig.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:3000/api"; // Backend URL
-// export const API_BASE_URL = "https://whatsappmessaginbot.onrender.com/api";
+// export const API_BASE_URL = "http://localhost:3000/api"; // Backend URL
+export const API_BASE_URL = "https://whatsappmessaginbot.onrender.com/api";
 export const SIGNUP_ENDPOINT = `${API_BASE_URL}/users/signup`;
 export const SIGNIN_ENDPOINT = `${API_BASE_URL}/users/login`;
 export const USER_PROFILE_UPDATE_ENDPOINT = `${API_BASE_URL}/users/update`;
